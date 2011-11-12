22c3e4a09e8c28dbfeb8b46b3003021f5bab3abd
Danil Pismenny <danil@orionet.ru>
2011-11-12 10:41:07 +0400

    Options for auto_html

--- lib/auto_html/auto_html_for.rb
+++ lib/auto_html/auto_html_for.rb
@@ -11,7 +11,7 @@ module AutoHtmlFor
   end
 
   module ClassMethods
-    def auto_html_for(raw_attrs, &proc)
+    def auto_html_for(raw_attrs, options = {}, &proc)
       include AutoHtmlFor::InstanceMethods
       before_save :auto_html_prepare
 
@@ -36,7 +36,7 @@ module AutoHtmlFor
         end
         define_method("auto_html_prepare_#{raw_attr}") do
           self.send(raw_attr.to_s + suffix + "=", 
-            auto_html(self.send(raw_attr), self, &proc))
+            auto_html(self.send(raw_attr), options, self, &proc))
         end
       end
     end
--- lib/auto_html/base.rb
+++ lib/auto_html/base.rb
@@ -5,10 +5,10 @@ module AutoHtml
     AutoHtml::Builder.add_filter(name, &block)
   end
 
-  def auto_html(raw, object=nil, &proc)
+  def auto_html(raw, options = {}, object = nil, &proc)
     return "" if raw.blank?
     builder = Builder.new(raw, object)
-    result = builder.instance_eval(&proc)
+    result = builder.instance_exec(options, &proc)
     return raw if result.nil?
     result.respond_to?(:html_safe) ?
       result.html_safe :
